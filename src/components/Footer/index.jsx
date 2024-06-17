52a63367f4cb9714d923e72bdb622fb27e0f3f21
George Schultz <georgeschultz@outlook.com>
2024-06-17 04:17:33 -0700

    finished populating projects with links and added proper contact and social media links to the footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,28 +1,31 @@
 function Footer() {
-
-  // Replace links with social media profiles
   const icons = [
     {
       name: "fab fa-github",
-      link: "https://github.com/"
+      link: "https://github.com/Geoschu",
     },
     {
       name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/"
+      link: "https://www.linkedin.com/in/george-schultz-10881b311/",
     },
     {
       name: "fab fa-stack-overflow",
-      link: "https://stackoverflow.com/"
-    }
-  ]
+      link: "https://stackoverflow.com/users/25603935/george-schultz",
+    },
+  ];
 
   return (
     <footer className="flex-row px-1">
-      {icons.map(icon =>
-      (
-        <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer"><i className={icon.name}></i></a>
-      )
-        )}
+      {icons.map((icon) => (
+        <a
+          href={icon.link}
+          key={icon.name}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className={icon.name}></i>
+        </a>
+      ))}
     </footer>
   );
 }
