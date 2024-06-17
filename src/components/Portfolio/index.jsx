fd8dbf344fba62759fb927dc896d228f12e4a349
George Schultz <georgeschultz@outlook.com>
2024-06-17 02:04:00 -0700

    updated project list

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,39 +1,116 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Project from "../Project";
 
 function Portfolio() {
-
-  // Replace links with deployed projects and GitHub repos
+  // links to projects and repos
   const [projects] = useState([
     {
-      name: 'surfReport',
-      description: 'MERN Stack',
+      name: "Semantic Refactor",
+      description: "HTML/CSS",
       link: "https://github.com",
-      repo: "https://github.com"
+      repo: "https://github.com",
     },
     {
-      name: 'pastelPuzzels',
-      description: 'MERN Stack',
+      name: "Password Generator",
+      description: "HTML/CSS",
       link: "https://github.com",
-      repo: "https://github.com"
+      repo: "https://github.com",
     },
     {
-      name: 'runBuddy',
-      description: 'HTML/CSS',
+      name: "Code Quiz",
+      description: "HTML/CSS",
       link: "https://github.com",
-      repo: "https://github.com"
+      repo: "https://github.com",
     },
     {
-      name: 'ledWall',
-      description: 'Node/IoT',
+      name: "Day Scheduler",
+      description: "HTML/CSS",
       link: "https://github.com",
-      repo: "https://github.com"
+      repo: "https://github.com",
     },
     {
-      name: 'calculator',
-      description: 'React/JavaScript/CSS',
+      name: "Weather Dashboard",
+      description: "React/JavaScript/CSS",
       link: "https://github.com",
-      repo: "https://github.com"
+      repo: "https://github.com",
+    },
+    {
+      name: "ParkPal",
+      description: "React/JavaScript/CSS",
+      link: "https://github.com",
+      repo: "https://github.com",
+    },
+    {
+      name: "Professional ReadMe Generator",
+      description: "React/JavaScript/CSS",
+      link: "https://github.com",
+      repo: "https://github.com",
+    },
+    {
+      name: "SVG Logo Maker",
+      description: "React/JavaScript/CSS",
+      link: "https://github.com",
+      repo: "https://github.com",
+    },
+    {
+      name: "Note Taker",
+      description: "React/JavaScript/CSS",
+      link: "https://github.com",
+      repo: "https://github.com",
+    },
+    {
+      name: "Employee Tracker",
+      description: "React/JavaScript/CSS",
+      link: "https://github.com",
+      repo: "https://github.com",
+    },
+    {
+      name: "E-Commerce Backend",
+      description: "React/JavaScript/CSS",
+      link: "https://github.com",
+      repo: "https://github.com",
+    },
+    {
+      name: "Tech Blog",
+      description: "React/JavaScript/CSS",
+      link: "https://github.com",
+      repo: "https://github.com",
+    },
+    {
+      name: "ReelRoster",
+      description: "React/JavaScript/CSS",
+      link: "https://github.com",
+      repo: "https://github.com",
+    },
+    {
+      name: "Regex Tutorial",
+      description: "React/JavaScript/CSS",
+      link: "https://github.com",
+      repo: "https://github.com",
+    },
+    {
+      name: "Social Media API",
+      description: "React/JavaScript/CSS",
+      link: "https://github.com",
+      repo: "https://github.com",
+    },
+    {
+      name: "Progressive Web App Text Editor",
+      description: "React/JavaScript/CSS",
+      link: "https://github.com",
+      repo: "https://github.com",
+    },
+    {
+      name: "React Portfolio",
+      description: "React/JavaScript/CSS",
+      link: "https://github.com",
+      repo: "https://github.com",
+    },
+    {
+      name: "Book Search Engine",
+      description: "React/JavaScript/CSS",
+      link: "https://github.com",
+      repo: "https://github.com",
     },
   ]);
 
@@ -41,14 +118,11 @@ function Portfolio() {
     <div>
       <div className="flex-row">
         {projects.map((project, idx) => (
-          <Project
-            project={project}
-            key={"project" + idx}
-          />
+          <Project project={project} key={"project" + idx} />
         ))}
       </div>
     </div>
   );
-};
+}
 
 export default Portfolio;
