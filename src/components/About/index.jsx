5a91b97970364dfd3deca5a11102cfd6a0477110
George Schultz <georgeschultz@outlook.com>
2024-06-17 16:15:47 -0700

    added more screenshots and replaced more boilerplate text

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,13 +3,24 @@ function About() {
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+          <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
         </div>
         <p>
-        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch Etsy authentic Marfa quinoa typewriter plaid direct trade small batch wayfarers bicycle rights cliche craft beer gastropub single-origin coffee Godard Carles you probably haven't heard of them irony pickled kitsch synth sriracha gentrify literally heirloom blog Truffaut paleo scenester
+          Hello! I'm a software engineer with a passion for problem-solving and
+          a strong interest in computer science. I have experience in front-end
+          and back-end development, and I'm always eager to learn new
+          technologies and improve my skills. In my free time, I enjoy building
+          open-source projects and exploring the latest trends in the tech
+          industry.
         </p>
         <p>
-        Echo Park sriracha Pinterest vegan biodiesel 90's irony iPhone Kickstarter Carles crucifix kitsch narwhal dreamcatcher pickled trust fund selvage art party letterpress Tumblr post-ironic kogi Thundercats Tonx Brooklyn Pitchfork Odd Future authentic normcore freegan leggings Schlitz chambray organic tousled retro fap squid street art church-key fashion axe Wes Anderson bespoke whatever gentrify banh mi you probably haven't heard of them American Apparel next level pug ugh.
+          I have worked on several projects, utilizing technologies such as
+          React, Node.js, and MongoDB. I am proficient in JavaScript, Python,
+          and Java. I have a strong understanding of data structures and
+          algorithms, and I am experienced in designing and implementing RESTful
+          APIs. I am also familiar with cloud platforms like AWS and GCP. I am a
+          team player, and I believe in the power of collaboration to solve
+          complex problems.
         </p>
       </div>
     </section>
